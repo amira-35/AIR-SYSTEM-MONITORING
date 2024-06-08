190ddea6512935efa320affb40526cb2844d3989
amira-35 <83829837+amira-35@users.noreply.github.com>
2024-06-08 22:04:45 +0600

    up

--- air_monitoring_app/air_monitoring_app/lib/main.dart
+++ air_monitoring_app/air_monitoring_app/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:AirNow/Controllers/my_functions.dart';
 import 'package:AirNow/firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'Views/AlertsScreen.dart';
+import 'package:latlong2/latlong.dart';
 import 'Views/MapScreen.dart';
 import 'Views/StatisticsScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,9 +36,87 @@ class _BottomNavBarState extends State<BottomNavBar> {
 
   final List<Widget> _tabs = [
     MapScreen(),
-    Alerts(),
     AirQualityChart(),
   ];
+
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeNotification();
+    _checkAirQuality();
+  }
+
+  void _initializeNotification() {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    final InitializationSettings initializationSettings = InitializationSettings(
+      android: initializationSettingsAndroid,
+    );
+
+    flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  Future<void> _checkAirQuality() async {
+    Position position = await _getCurrentPosition();
+    LatLng _currentPosition = LatLng(position.latitude, position.longitude);
+    
+    double? aqi = await getAQIFromFirebase(_currentPosition);
+
+    if (aqi != null && aqi > 4) {
+      _sendNotification('Alert', 'Attention, your location is very polluted!');
+    }
+  }
+
+  Future<Position> _getCurrentPosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future<void> _sendNotification(String title, String body) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+        AndroidNotificationDetails(
+      'your_channel_id',
+      'your_channel_name',
+      channelDescription: 'your_channel_description',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+        NotificationDetails(android: androidPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'item x',
+    );
+  }
 
   void _onTabTapped(int index) {
     setState(() {
@@ -46,10 +128,12 @@ class _BottomNavBarState extends State<BottomNavBar> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Air current quality'),
+        title: const Text('Air Current Quality'),
         leading: IconButton(
           icon: const Icon(Icons.air),
-          onPressed: () { },
+          onPressed: () {
+            // Action à effectuer lors du clic sur l'icône de position
+          },
         ),
       ),
       body: _tabs[_currentIndex],
@@ -60,10 +144,6 @@ class _BottomNavBarState extends State<BottomNavBar> {
           BottomNavigationBarItem(
             icon: Icon(Icons.map),
             label: 'Map',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.warning),
-            label: 'Alerts',
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.bar_chart),
